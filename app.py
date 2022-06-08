62729bc9a38d472ace19c4a531ae2cf42255d28d
Mohammad Abdullah <abdullah_22@hotmail.com>
2022-06-08 16:01:43 +0100

    Create basic flask server

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+   port = int(os.environ.get("POST", 5000))
+   app.run(debug=True, host='0.0.0.0', port=port)
